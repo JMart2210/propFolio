277a13a1d36f4f04f8592edeeaa4e80e9adc21a7
Jeremy Martin <46061444+JMart2210@users.noreply.github.com>
2023-04-26 15:19:52 -0400

    added loading state for chart

--- public/propertyAPI.js
+++ public/propertyAPI.js
@@ -1,11 +1,37 @@
-const ctx = document.getElementById('myChart');
+let ctx = document.getElementById('myChart');
+let placeholder = document.getElementById('chartPlaceholder');
 const propertyId = window.location.pathname.split("/")[2]; // Extract property ID from URL
+
+// let madeAChart = new Chart(ctx, {
+//   type: 'bar',
+//   data: {
+//     labels: [],
+//     datasets: [{
+//       label: 'Property Values',
+//       data: [],
+//       borderWidth: 1
+//     }]
+//   },
+//   options: {
+//     scales: {
+//       y: {
+//         beginAtZero: true
+//       },
+//       x: {
+//         reverse: false
+//       }
+//     }
+//   }
+// });
+placeholder.innerHTML = 'loading ...'
 fetch(`/api/data/${propertyId}`)
   .then(response => response.json())
   .then(data => {
     const dates = data.map((data) => dayjs(data.date).format('MMM D, YYYY')).reverse();
     const values = data.map((data) => data.value).reverse();
-    
+    // madeAChart.destroy()
+    // placeholder.innerHTML = 'finished'
+    placeholder.remove()
     new Chart(ctx, {
       type: 'bar',
       data: {
