1533c413502ece035c7b431cfc724c2fc8dcba16
Jeremy Martin <jeremy@blndhomes.com>
2024-08-15 13:49:10 -0400

    Version 2 implementation

--- server/middleware/attom.js
+++
@@ -1,4 +0,0 @@
-require("dotenv").config({ path: "./config/.env" });
-
-
-api_key: process.env.ATTOM_API_KEY
--- server/routes/api.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const upload = require("../middleware/multer");
-const apiController = require("../controllers/api");
-const { ensureAuth, ensureGuest } = require("../middleware/auth");
-
-router.get("/data/:id", ensureAuth, apiController.getValues);
-
-module.exports = router;
