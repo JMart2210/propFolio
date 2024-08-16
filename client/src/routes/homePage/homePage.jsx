5d492a7d5b053986c8eb4766157806c44e98f3ea
Jeremy Martin <jeremy@blndhomes.com>
2024-08-16 07:26:23 -0400

    updated client

--- client/src/routes/homePage/homePage.jsx
+++ client/src/routes/homePage/homePage.jsx
@@ -1,12 +1,17 @@
+import { useContext } from "react";
 import SearchBar from "../../components/searchBar/SearchBar";
 import "./homePage.scss";
+import { AuthContext } from "../../context/AuthContext";
 
 function HomePage() {
+
+  const {currentUser} = useContext(AuthContext)
+
   return (
     <div className="homePage">
       <div className="textContainer">
         <div className="wrapper">
-          <h1 className="title">A Real Estate Marketplace</h1>
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
             explicabo suscipit cum eius, iure est nulla animi consequatur
